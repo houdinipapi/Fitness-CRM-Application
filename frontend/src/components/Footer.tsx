afd425553be6955493d56bd5bca4dbf8fc2d4227
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-10 20:13:57 +0300

    chore: Update Footer component
    The Footer component has been updated to include a new layout and styling. The changes include adding a container with centered text and a list of links for navigation. Additionally, the copyright information has been added. This update enhances the overall layout and completeness of the website.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-blue-500 py-4 w-full relative bottom-0">
+        <div className="container mx-auto px-4 text-center font-semibold">
+            
+            <ul className="mt-2 flex justify-center space-x-6">
+                <li className="hover:text-blue-700">
+                    <a href="#">HOME</a>
+                </li>
+                <li className="hover:text-blue-700">
+                    <a href="#">BLOG</a>
+                </li>
+                <li className="hover:text-blue-700">
+                    <a href="#">STATUS</a>
+                </li>
+            </ul>
+
+            <p>
+                © {new Date().getFullYear()} ACHIOMWA. All rights reserved.
+            </p>
+        </div>
+    </footer>
   )
 }
 
