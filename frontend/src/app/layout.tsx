02f901983ee86e413c5a44431b8aa71d89bb3117
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-10 19:57:04 +0300

    chore: Add Footer component
    Add a new Footer component to the project. This component will be used in the RootLayout component to display a footer section at the bottom of the page. The Footer component includes a simple "Footer" text element. This addition enhances the overall layout and completeness of the website.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={raleway.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -5,8 +5,8 @@ const LandingPage = () => {
   return (
     <div className="container mx-auto max-w-screen-xl px-16">
 
-      <div>
-        <h1 className="font-bold text-xl py-2">FitNess for Every Body</h1>
+      <div className="">
+        <h1 className="font-bold text-xl py-2">FITNESS FOR EVERY BODY</h1>
         <p className="font-semibold pb-2">
           Welcome to FitNess! We&apos;re here to help you get in shape and feel great. <br />
           Whether you&apos;re a seasoned athlete or just starting out, we have everything you need to reach your fitness goals.
@@ -15,10 +15,10 @@ const LandingPage = () => {
 
       <div className="flex justify-center items-center mt-8 mb-16">
         {/* First */}
-        <a href="#" className="flex-1 relative cursor-pointer flex flex-col bg-[#FBE9E7] rounded-2xl mr-8 transition duration-300 hover:shadow-lg hover:scale-105">
-          <div className="p-12 pt-4 pb-4">
-            <h1 className="font-bold text-[22px] text-orange-600">For Fitness Businesses</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, veniam!</p>
+        <a href="#" className="flex-1 relative cursor-pointer flex flex-col bg-red-100 rounded-2xl mr-8 transition duration-300 hover:bg-red-200 hover:shadow-lg hover:scale-105">
+          <div className="p-12 py-4">
+            <h1 className="font-bold text-[22px] text-orange-600">Businesses</h1>
+            <p className="font-semibold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, veniam!</p>
           </div>
 
           <div className="relative h-[400px] mt-auto">
@@ -33,10 +33,10 @@ const LandingPage = () => {
         </a>
 
         {/* Second */}
-        <a href="#" className="flex-1 relative cursor-pointer flex flex-col bg-[#E3F2FD] rounded-2xl mr-8 transition duration-300 hover:shadow-lg hover:scale-105">
-          <div className="p-12 pt-4 pb-4">
-            <h1 className="font-bold text-[22px] text-blue-600">For Corporates</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, veniam!</p>
+        <a href="#" className="flex-1 relative cursor-pointer flex flex-col bg-blue-200 rounded-2xl mr-8 transition duration-300 hover:bg-blue-300 hover:shadow-lg hover:scale-105">
+          <div className="p-12 py-4">
+            <h1 className="font-bold text-[22px] text-blue-600">Corporates</h1>
+            <p className="font-semibold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, veniam!</p>
           </div>
           <div className="relative h-[400px] mt-auto">
             <Image
@@ -50,10 +50,10 @@ const LandingPage = () => {
         </a>
 
         {/* Third */}
-        <a href="#" className="flex-1 relative cursor-pointer flex flex-col bg-[#EDE7F6] rounded-2xl transition duration-300 hover:shadow-lg hover:scale-105">
-          <div className="p-12 pt-4 pb-4">
-            <h1 className="font-bold text-[22px] text-purple-700">For Fitness Businesses</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, veniam!</p>
+        <a href="#" className="flex-1 relative cursor-pointer flex flex-col bg-purple-200 rounded-2xl transition duration-300 hover:bg-purple-300 hover:shadow-lg hover:scale-105">
+          <div className="p-12 py-4">
+            <h1 className="font-bold text-[22px] text-purple-700">Personalize</h1>
+            <p className="font-semibold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, veniam!</p>
           </div>
           <div className="relative h-[400px] mt-auto">
             <Image
