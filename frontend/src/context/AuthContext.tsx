89f58d5e09c763153072dd771aa0971647d2bf41
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-13 21:54:11 +0300

    refactor: Add checkAuth function to AuthProvider
    This refactor adds a checkAuth function to the AuthProvider component in the AuthContext file. The checkAuth function retrieves the access token from local storage and updates the isAuthenticated state based on its presence. This change improves the authentication functionality by ensuring that the user's authentication status is accurately reflected throughout the application.

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,18 +1,23 @@
 "use client"
 
-import React, { createContext, useState, useContext, ReactNode } from "react";
+import React, { createContext, useState, useContext, ReactNode, useEffect } from "react";
 
 
 interface AuthContextType {
     isAuthenticated: boolean;
     login: () => void;
     logout: () => void;
+    checkAuth: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    useEffect(() => {
+        checkAuth();
+    }, [])
 
     const login = () => {
         setIsAuthenticated(true);
@@ -24,8 +29,13 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         localStorage.removeItem("refresh");
     };
 
+    const checkAuth = () => {
+        const access = localStorage.getItem("access");
+        setIsAuthenticated(!!access);
+    }
+
     return (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout, checkAuth }}>
             {children}
         </AuthContext.Provider>
     );
